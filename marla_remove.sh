25715386ecc384165fd58f774445c3dabc5cee58
vigial <vicent.gimenez.alventosa@gmail.com>
2017-05-27 20:15:57 +0200

    Add files via upload
    new bash scripts use auxiliar directorys in $HOME/.marla/clustername to store .json files, functions and zip packages

--- marla_remove.sh
+++ marla_remove.sh
@@ -1,55 +1,35 @@
- #!/bin/bash
-
-# MARLA - MApReduce on AWS Lambda
-# Copyright (C) GRyCAP - I3M - UPV 
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License. 
-
-
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 cluster-name"
-  exit -1
-fi
-
+ 
 CLUSTERNAME=$1
 
-rm stderr &> /dev/null
+rm $HOME/.marla/$CLUSTERNAME/stderr &> /dev/null
 if aws lambda delete-function --function-name HC-$CLUSTERNAME-lambda-mapper
 then
     echo "Mapper function removed from cluster '$CLUSTERNAME'"
 else
     echo "Error removing mapper function from cluster '$CLUSTERNAME'"
-    more stderr
+    more $HOME/.marla/$CLUSTERNAME/stderr
     exit 1
 fi
 
-rm stderr &> /dev/null
+rm $HOME/.marla/$CLUSTERNAME/stderr &> /dev/null
 if aws lambda delete-function --function-name HC-$CLUSTERNAME-lambda-reducer
 then
     echo "Reducer function removed from cluster '$CLUSTERNAME'"
 else
     echo "Error removing reducer function from cluster '$CLUSTERNAME'"
-    more stderr
+    more $HOME/.marla/$CLUSTERNAME/stderr
     exit 1
 fi
 
 
-rm stderr &> /dev/null
+rm $HOME/.marla/$CLUSTERNAME/stderr &> /dev/null
 if aws lambda delete-function --function-name HC-$CLUSTERNAME-lambda-coordinator
 then
     echo "Coordinator function removed from cluster '$CLUSTERNAME'"
 else
     echo "Error removing coordinator function from cluster '$CLUSTERNAME'"
-    more stderr
+    more $HOME/.marla/$CLUSTERNAME/stderr
     exit 1
 fi
+
+rm -r $HOME/.marla/$CLUSTERNAME &> /dev/null
